3c0a4f219d7fbb8a415685b8efac9644c08aa167
Jorge <jorge@MacBookPro.technicolor.net>
2026-03-09 15:42:52 +0100

    this is my commit!!

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Welcome to my awesome Flask project!'
+    return 'Hello from Flask - this is amazing!'
 
 if __name__ == '__main__':
     app.run(debug=True)
